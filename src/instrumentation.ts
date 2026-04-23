14087a2f63f631fee82e16235a7dc0c2fb518a64
AhmedHossam777 <ahmedhossam9226@gmail.com>
2026-04-23 17:08:55 +0200

    feat:establish db connection

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,5 +1,7 @@
 export async function register() {
   if (process.env.NEXT_RUNTIME === "nodejs") {
     await import("@/lib/config");
+    const { connectDB } = await import("@/lib/db/connection");
+    await connectDB();
   }
 }
